8f6304f287a498922e30628e54cc4a46eab1bdd1
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-29 14:18:21 +0000

    update

--- good-movies/components/about/Home.jsx
+++ good-movies/components/about/Home.jsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import styles from './Home.module.scss'
 function Home() {
     return (
-        <main className="text-light" role="main">
+        <main className="text-light home" role="main">
 
             <div className="jumbotron">
-                <div className="container">
+                <div className={styles.container}>
                     <h1 className="display-3">GOOD MOVIES</h1>
                     <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
                     <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
