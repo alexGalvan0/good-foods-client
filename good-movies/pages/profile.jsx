b0521b45ef1023fe131c06483abd9f75bbcaad8e
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-12-01 01:50:25 +0000

    update

--- good-movies/pages/profile.jsx
+++ good-movies/pages/profile.jsx
@@ -1,5 +1,6 @@
 import useUser from "../hooks/useUser"
 import Link from 'next/link'
+import Image from 'next/image'
 import { useState, useEffect } from "react"
 import axios from "axios"
 
@@ -7,22 +8,25 @@ function Profile() {
     let user = useUser()
     const url = `http://127.0.0.1:8000/api/getUserLikedMovies/${user.id}`
 
-    const [data, setData] = useState('')
+    const [data, setData] = useState([])
 
     useEffect(() => {
         const getData = async () => {
-            console.log(user)
             const request =  await axios.get(url)
             const response = request
             setData(response.data)
         }
-        getData()
+        try{
+            getData()
+        }catch{
+             return
+        }
     }, [user])
 
 
     return ( 
         <div className="container mt-5">
-            <div className="row">
+            <div className="row"> 
                 <div className="col">
                     <div className="p-5 mb-4 bg-primary rounded-3">
                         <div className="container-fluid py-5">
@@ -31,9 +35,13 @@ function Profile() {
                             <Link href='movie/search'><button className="btn btn-alert btn-lg text-light" type="button">Search Movies</button></Link>
                         </div>
                     </div>
+
+                    {data.map((m) => (
+                                <Image src={m.poster} alt='' height={200} width={150}/>
+                            ))}
                 </div>
-            </div>
+            </div> 
         </div>
     )
 }
-export default Profile
+export default Profile 
