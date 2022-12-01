7970944c6ffcce881b2982cab1199c2fa535f2f4
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-12-01 01:17:57 +0000

    update

--- good-movies/components/movies/ButtonGroup.jsx
+++ good-movies/components/movies/ButtonGroup.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useState } from "react";
+import useUser from "../../hooks/useUser";
 
-function ButtonGroup({ mData, uData }) {
+function ButtonGroup({mData}) {
+    const user = useUser()
     const url = 'http://127.0.0.1:8000/api/movies/'
 
     const [movieData, setMovieData] = useState([])
@@ -18,39 +20,35 @@ function ButtonGroup({ mData, uData }) {
         imdbId: mData.imdbID,
         year: mData.Year,
         //roten_score:mData.Ratings.toString(),
-        run_time:mData.Runtime
+        run_time: mData.Runtime
 
-    }
-
-    const getMoviesDb = async () => {
-        const data = axios.get(url)
-        const response = await data
-        setMovieData(response)
-        return (movieData)
     }
 
     const addMovieToDb = async () => {
-       let getMoviesInDb = getMoviesDb()
-       let movieInDb = getMoviesInDb.length
-
-        if (movieInDb === 0){
-            let request = axios.post(url,config)
-            let response = await request
-            console.log(response)
-
-       } else {
-        console.log('movieInDb')
-       }
-
+        console.log(mData)
+        try{
+        await axios.post(url, config)
+        } catch {
+            return
+        }
+        
+    }
+    const likeMovie = async () => {
+        await addMovieToDb()
+        let request = axios.post(`http://127.0.0.1:8000/api/addLikedList/${user.id}/${mData.imdbID}/`)
+        let response = await request
+        console.log(response.data)
     }
 
-    return (
-        <div className="btn-group gap-1 bg-dark" role="group" aria-label="Basic example">
-            <button type="button" onClick={addMovieToDb} className="btn btn-alert text-light">Like</button>
-            <button type="button" className="btn btn-alert text-light">Add Watch List</button>
-            <button type="button" className="btn btn-alert text-light">Review</button>
-        </div>
-    )
+
+
+return (
+    <div className="btn-group gap-1 bg-dark" role="group" aria-label="Basic example">
+        <button type="button" onClick={likeMovie} className="btn btn-alert text-light">Like</button>
+        <button type="button" className="btn btn-alert text-light">Add Watch List</button>
+        <button type="button" className="btn btn-alert text-light">Review</button>
+    </div>
+)
 }
 
 export default ButtonGroup;
--- good-movies/pages/profile.jsx
+++ good-movies/pages/profile.jsx
@@ -1,17 +1,33 @@
 import useUser from "../hooks/useUser"
 import Link from 'next/link'
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 function Profile() {
     const user = useUser()
+    const url = `http://127.0.0.1:8000/api/getUserLikedMovies/${26}`
+    const [data, setData] = useState('')
+
+    useEffect(() => {
+        let userId = user.id
+        const getData = async () => {
+            const request = axios.get(url)
+            const response = await request
+            console.log(user.id)
+        }
+        getData()
+    }, [])
+
+
     return (
         <div className="container mt-5">
             <div className="row">
                 <div className="col">
-                    <div class="p-5 mb-4 bg-primary rounded-3">
-                        <div class="container-fluid py-5">
-                            <h1 class="display-5 fw-bold text-light">Hello {user.first_name}!</h1>
+                    <div className="p-5 mb-4 bg-primary rounded-3">
+                        <div className="container-fluid py-5">
+                            <h1 className="display-5 fw-bold text-light">Hello {user.first_name}!</h1>
 
-                            <Link href='movie/search'><button class="btn btn-alert btn-lg text-light" type="button">Search Movies</button></Link>
+                            <Link href='movie/search'><button className="btn btn-alert btn-lg text-light" type="button">Search Movies</button></Link>
                         </div>
                     </div>
                 </div>
