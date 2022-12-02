8345f261f34c95482e23819d342c9f86db3fea7e
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-12-02 23:58:32 +0000

    loggin

--- good-movies/pages/login.jsx
+++ good-movies/pages/login.jsx
@@ -1,14 +1,17 @@
 import LoginForm from "../components/login/LoginForm"
 import GoogleLogin from "../components/login/GoogleLogin";
+import SignIn from "../components/login/SignIn";
 function Login() {
     return (
-        <div className="container flex-column justify-content-center mt-5 pt-5">
-            <h2 className="text-light">Login</h2>
+        <div className="container flex-column justify-content-center mt-xl-5 pt-xl-5">
+
             <div className="row">
                 <div className="d-flex justify-content-around flex-column flex-md-row p-5">
                     <div className="pb-5 text-center">
-                        <LoginForm />
+                        {/* <LoginForm /> */}
+                        <SignIn />
                         {/* <GoogleLogin /> */}
+
                     </div>
                 </div>
             </div>
