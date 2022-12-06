bd00e85df725a0dc115a82b42b4215d462fac158
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-12-06 20:59:40 +0000

    update

--- good-movies/components/nav/SnackBarRegister.jsx
+++ good-movies/components/nav/SnackBarRegister.jsx
@@ -24,7 +24,7 @@ export default function SnackbarAlert({
       uName == "" ||
       fName == "" ||
       lName == "" ||
-      eMail == "" ||
+      !eMail.includes('@','.com') ||
       passWord == ""
       
     )
