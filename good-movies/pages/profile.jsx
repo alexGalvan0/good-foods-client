dba9789c43f09c368fd6c2d2c9f7edc22ffa3885
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-12-07 14:52:02 +0000

    added movies

--- good-movies/pages/profile.jsx
+++ good-movies/pages/profile.jsx
@@ -3,22 +3,21 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Button from '@mui/material/Button';
-import FollowCard from '../components/follows/FollowCard'
+import Button from "@mui/material/Button";
+import FollowCard from "../components/follows/FollowCard";
 import { Container, Typography } from "@mui/material";
 
 function Profile() {
-
   let user = useUser();
   const url = `https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/getUserLikedMovies/${user.id}/`;
-  const following = `https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/follow/${user.id}/${user.username}/`
+  const following = `https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/follow/${user.id}/${user.username}/`;
 
   const [data, setData] = useState([]);
   const [followData, setFollowData] = useState([]);
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
-    const config = { headers: { Authorization: `Bearer ${token}` } }
+    const token = localStorage.getItem("token");
+    const config = { headers: { Authorization: `Bearer ${token}` } };
 
     const getData = async () => {
       const request = await axios.get(url, config);
@@ -26,26 +25,27 @@ function Profile() {
       setData(response);
     };
     const getFollowing = async () => {
-      const request = await axios.get(following, config)
-      const response = request.data
-      setFollowData(response)
-      console.log(response)
-    }
+      const request = await axios.get(following, config);
+      const response = request.data;
+      setFollowData(response);
+      console.log(response);
+    };
 
     const getAllUserData = () => {
-      getData()
-      getFollowing()
-    }
-    user.id != undefined ? getAllUserData() : null
-
+      getData();
+      getFollowing();
+    };
+    user.id != undefined ? getAllUserData() : null;
   }, [user]);
 
   const deleteLikedMovie = async (imdbId) => {
-    await axios.delete(`https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/addLikedList/${user.id}/${imdbId}/`);
+    await axios.delete(
+      `https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/addLikedList/${user.id}/${imdbId}/`
+    );
     const request = await axios.get(url);
     const response = request;
     setData(response.data);
-  }
+  };
 
   return (
     <div className="container mt-3">
@@ -59,10 +59,7 @@ function Profile() {
               <h6 className="text-light"> Movies Liked: {data.length}</h6>
 
               <Link href="movie/search">
-                <Button
-                  sx={{ bgcolor: 'primary.main' }}
-                  color='secondary'
-                >
+                <Button sx={{ bgcolor: "primary.main" }} color="secondary">
                   Search Movies
                 </Button>
               </Link>
@@ -72,19 +69,18 @@ function Profile() {
       </div>
 
       <div className="row">
-        <Container sx={{ bgcolor: 'secondary.grey' }}>
-
-          <Typography color='secondary.main' variant={'h5'}>Liked Movies: </Typography>
-
+        <Container sx={{ bgcolor: "secondary.grey" }}>
+          <Typography color="secondary.main" variant={"h5"}>
+            Liked Movies:{" "}
+          </Typography>
         </Container>
         <Container
           className=" d-flex gap-3  mb-3 py-2 "
-          sx={{ bgcolor: 'secondary.grey' }}
+          sx={{ bgcolor: "secondary.grey" }}
           style={{
             maxHeight: "40rem",
             overflow: "hidden",
             overflow: "auto",
-
           }}
         >
           {Array.isArray(data) &&
@@ -92,26 +88,37 @@ function Profile() {
               <div className="col d-flex flex-column">
                 <div>
                   <Button
-                    sx={{ marginBottom: -4.5, paddingLeft: 17.5, paddingRight: 0 }}
+                    sx={{
+                      marginBottom: -4.5,
+                      paddingLeft: 17.5,
+                      paddingRight: 0,
+                    }}
                     varient="text"
-                    color='alert'
+                    color="alert"
                     size="large"
-                    onClick={() => { deleteLikedMovie(m.imdbId) }}
+                    onClick={() => {
+                      deleteLikedMovie(m.imdbId);
+                    }}
                     className="btn-alert"
                   >
                     x
                   </Button>
-                  <Link style={{ textDecoration: 'none' }} href={`/movie/${m.imdbId}`}><div className="d-flex text-center">
-                    <Image key={m.imdbId} src={m.poster} alt="" height={200} width={150} />
-                    <div
-                      className="d-flex  flex-column align-items-center"
-                    >
-
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    href={`/movie/${m.imdbId}`}
+                  >
+                    <div className="d-flex text-center">
+                      <Image
+                        key={m.imdbId}
+                        src={m.poster}
+                        alt=""
+                        height={200}
+                        width={150}
+                      />
+                      <div className="d-flex  flex-column align-items-center"></div>
                     </div>
-                  </div>
                   </Link>
-                  <div>
-                  </div>
+                  <div></div>
                 </div>
               </div>
             ))}
@@ -119,18 +126,18 @@ function Profile() {
       </div>
       <div className="row pb-5 mb-5">
         <div className="col px-xl-5  d-flex flex-column justify-content-center">
-          <Container sx={{ bgcolor: 'secondary.grey' }} className='rounded'>
-            <Typography color='secondary.main' variant={'h5'}>Following: </Typography>
+          <Container sx={{ bgcolor: "secondary.grey" }} className="rounded">
+            <Typography color="secondary.main" variant={"h5"}>
+              Following:{" "}
+            </Typography>
           </Container>
 
-          <div className="col mt-2 px-xl-5 d-flex flex-wrap gap-3" >
+          <div className="col mt-2 px-xl-5 d-flex flex-wrap gap-3">
             {followData.map((f) => (
               <div className="col">
                 <FollowCard key={f.id} userName={f.username} userId={f.id} />
               </div>
-
             ))}
-
           </div>
         </div>
       </div>
--- good-movies/pages/user/[id].jsx
+++ good-movies/pages/user/[id].jsx
@@ -1,38 +1,43 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { Typography } from "@mui/material";
-import Container from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import Link from "next/link";
-import Image from 'next/image'
+import Image from "next/image";
 
 function FollowingProfile() {
-    const [data, setData] = useState([]);
-    const router = useRouter()
-    const [userData, setUserData] = useState([])
-    const { id } = router.query;
-    const BASE_URL = 'https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/'
-    const url = `https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/getUserLikedMovies/${id}/`;
+  const [data, setData] = useState([]);
+  const router = useRouter();
+  const [userData, setUserData] = useState([]);
+  const { id } = router.query;
+  const BASE_URL =
+    "https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/";
+  const url = `https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/getUserLikedMovies/${id}/`;
 
+  useEffect(() => {
+    const getData = async () => {
+      const request = await axios.get(url);
+      const response = request.data;
+      setData(response);
+    };
+    getData();
+  }, []);
 
-    useEffect(() =>{
-
-        const getData = async () => {
-            const request = await axios.get(url);
-            const response = request.data;
-            setData(response);
-          };
-          getData()
-    },[])
-
-    return(
-        <>
-        {data.map((d)=>(
-          <Link href={`/movie/${d.imdbId}`}>
-          <Image key={d.id} src={d.poster} height={200} width={150} alt='poster'></Image>
-          </Link>
-        ))}
-      </>
-    )
+  return (
+    <Container>
+      <Typography>Profile</Typography>
+      {data.map((d) => (
+        <Link href={`/movie/${d.imdbId}`}>
+          <Image
+            src={d.poster}
+            key={d.id}
+            alt={d.title}
+            height={200}
+            width={150}
+          />
+        </Link>
+      ))}
+    </Container>
+  );
 }
-export default FollowingProfile 
+export default FollowingProfile;
