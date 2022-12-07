8be908ec838007efb7562f17697e7b45fae62ce7
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-12-07 17:44:12 +0000

    update

--- good-movies/pages/user/[id].jsx
+++ good-movies/pages/user/[id].jsx
@@ -1,17 +1,20 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { Container, Typography,Box } from "@mui/material";
+import { Container, Typography, Box } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
 
 function FollowingProfile() {
-  const [data, setData] = useState([]);
   const router = useRouter();
+  const [data, setData] = useState([]);
+  const [watched, setWatched] = useState([]);
   const [userData, setUserData] = useState([]);
+
   const { id } = router.query;
   const BASE_URL = `https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/following/${id}/`;
   const url = `https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/getUserLikedMovies/${id}/`;
+  const watchUrl = `https://8000-alexgalvan0-goodmoviesa-b4acnd9aawy.ws-us77.gitpod.io/api/getUserWatchedMovies/${id}/`;
 
   useEffect(() => {
     const getData = async () => {
@@ -24,14 +27,20 @@ function FollowingProfile() {
       const response = request.data;
       setUserData(response);
     };
+    const getWatchedData = async () => {
+      const request = await axios.get(watchUrl);
+      const response = request.data;
+      setWatched(response);
+    };
 
     getUserData();
     getData();
+    getWatchedData();
   }, []);
 
   return (
     <Container>
-      <Box sx={{paddingBottom:3}}>
+      <Box sx={{ paddingBottom: 3 }}>
         <Typography variant="h2" color="secondary">
           @{userData.username}
         </Typography>
@@ -50,9 +59,23 @@ function FollowingProfile() {
           </Link>
         ))}
       </Box>
-      <Typography variant="h6" color="secondary">
-        MOVIES WATCHED:
-      </Typography>
+
+      <Box sx={{ paddingBottom: 3 }}>
+        <Typography variant="h6" color="secondary">
+          WATCHED
+        </Typography>
+        {watched.map((w) => (
+          <Link href={`/movie/${w.imdbId}`} key={w.id + "w"}>
+            <Image
+              src={w.poster}
+              key={w.id}
+              alt={w.title}
+              height={200}
+              width={150}
+            />
+          </Link>
+        ))}
+      </Box>
     </Container>
   );
 }
