c90ed3975435f6a620d1691ea944a4a37a2be603
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-12-10 12:44:30 +0000

    not working review post

--- good-movies/components/movies/ReviewText.jsx
+++ good-movies/components/movies/ReviewText.jsx
@@ -3,9 +3,11 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Link from "next/link";
 import { Button } from "@mui/material";
+import axios from "axios";
+import useUser from "../../hooks/useUser";
 
 
-export default function ReviewText({movieTitle}) {
+export default function ReviewText({movieTitle,mData}) {
   const [value, setValue] = React.useState("");
 
   const handleChange = (event) => {
@@ -16,6 +18,30 @@ export default function ReviewText({movieTitle}) {
 
 
   //ADDD API CALL TO POST MOVIE
+    //GET && POST REVIEWS https://8000-alexgalvan0-goodmoviesa-pjtmlhva1y5.ws-us78.gitpod.io/api/review/
+  // JSON SHAPE
+// {
+//   "id": 1,
+//   "review": "This movie is soooo good!",
+//   "user": 1,
+//   "movie": 10
+// }
+const user = useUser()
+
+const submitReview = async()=>{
+  if(value){
+    const url = 'https://8000-alexgalvan0-goodmoviesa-pjtmlhva1y5.ws-us78.gitpod.io/api/review/'
+    let config = {
+      id:user.id,
+      review:value,
+      movie: 1
+    }
+    let req = axios.post(url,config)
+    let res = await req
+    console.log(mData)
+
+  }
+}
 
   return (
     <Box
@@ -39,7 +65,7 @@ export default function ReviewText({movieTitle}) {
           <Button
             size="small"
             variant="contained"
-            // onClick={likeMovie}
+            onClick={submitReview}
             sx={{ bgcolor: "secondary" }}
           >
             ADD REVIEW
