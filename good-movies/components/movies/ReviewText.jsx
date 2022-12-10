bef940052e3cfc049ce3cee6d653d41f178bf6c7
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-12-10 21:56:29 +0000

    update

--- good-movies/components/movies/ReviewText.jsx
+++ good-movies/components/movies/ReviewText.jsx
@@ -6,60 +6,56 @@ import { Button } from "@mui/material";
 import axios from "axios";
 import useUser from "../../hooks/useUser";
 
-
-export default function ReviewText({movieTitle,mData}) {
+export default function ReviewText({ movieTitle, mData }) {
   const [value, setValue] = React.useState("");
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
-const user = useUser()
-const murl =
-"https://8000-alexgalvan0-goodmoviesa-pjtmlhva1y5.ws-us78.gitpod.io/api/movies/";
-const config = {
-  title: mData.Title,
-  plot: mData.Plot,
-  cast: mData.Actors,
-  poster: mData.Poster,
-  rated: mData.Rated,
-  director: mData.Director,
-  date_released: mData.Released,
-  imdbId: mData.imdbID,
-  year: mData.Year,
-  //roten_score:mData.Ratings.toString(),
-  run_time: mData.Runtime,
-};
+  const user = useUser();
+  const murl =
+    "https://8000-alexgalvan0-goodmoviesa-pjtmlhva1y5.ws-us78.gitpod.io/api/movies/";
+  const config = {
+    title: mData.Title,
+    plot: mData.Plot,
+    cast: mData.Actors,
+    poster: mData.Poster,
+    rated: mData.Rated,
+    director: mData.Director,
+    date_released: mData.Released,
+    imdbId: mData.imdbID,
+    year: mData.Year,
+    //roten_score:mData.Ratings.toString(),
+    run_time: mData.Runtime,
+  };
 
-const addMovieToDb = async () => {
-  await axios.post(murl, config).catch(function (err) {
-    return;
-  });
-};
+  const addMovieToDb = async () => {
+    await axios.post(murl, config).catch(function (err) {
+      return;
+    });
+  };
 
-const submitReview = async()=>{
-  if(value){
-    await addMovieToDb()
-    // GET MOVIE ID 
-    const movieIdUrl = `https://8000-alexgalvan0-goodmoviesa-pjtmlhva1y5.ws-us78.gitpod.io/api/getMovieByImdbID/${mData.imdbID}/`
-    let idRequest = axios.get(movieIdUrl)
-    let idResponse = await idRequest.data
-    console.log(idResponse)
+  const submitReview = async () => {
+    if (value) {
+      await addMovieToDb();
+      // GET MOVIE ID
+      const movieIdUrl = `https://8000-alexgalvan0-goodmoviesa-pjtmlhva1y5.ws-us78.gitpod.io/api/getMovieByImdbID/${mData.imdbID}/`;
+      let idRequest = axios.get(movieIdUrl);
+      let movieId = await idRequest.data;
 
-  //   const url = 'https://8000-alexgalvan0-goodmoviesa-pjtmlhva1y5.ws-us78.gitpod.io/api/review/'
-  //   let config = {
-  //     user:user.id,
-  //     review:value,
-  //     movie: mData.id
-  //   }
-  //   let req = axios.post(url,config)
-  //   let res = await req
-  //   console.log(mData)
-  //   console.log(res.data)
+      const url =
+        "https://8000-alexgalvan0-goodmoviesa-pjtmlhva1y5.ws-us78.gitpod.io/api/review/";
+      let config = {
+        user: user.id,
+        review: value,
+        movie: movieId,
+      };
+      let req = axios.post(url, config);
+      let res = await req;
 
-   }
-}
-
+    }
+  };
 
   return (
     <Box
