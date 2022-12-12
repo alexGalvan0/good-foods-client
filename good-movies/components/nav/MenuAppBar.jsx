effd51d8f25c65805eec7ef0eb84d0cc15ec8285
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-12-12 16:12:46 +0000

    update

--- good-movies/components/nav/MenuAppBar.jsx
+++ good-movies/components/nav/MenuAppBar.jsx
@@ -83,7 +83,6 @@ export default function MenuAppBar() {
                 <MenuItem onClick={(handleClose, goToProfile)}>
                   Profile
                 </MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
               </Menu>
             </div>
           )}
